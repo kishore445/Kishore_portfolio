d8550a6953ef2e7fb8a30c0a3c4e22c4f0b29758
Kishore_Tuggali <86900190+kishore445@users.noreply.github.com>
2024-06-19 19:59:25 +0530

    navnar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faHouse ,faUser,faDiagramProject,faFile} from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
   return (
-    <div className="flex flex-row  relative  items-center pl-9 py-9 bg-black ">
-         <Link to="/"><h3 className="text-white hover:scale-110 duration-100  top-5 px-20  right-80 absolute">Home</h3></Link>
-        <Link to="/"><h3 className="text-white hover:scale-110 duration-100 px-20  right-60 top-5 absolute">About</h3></Link>
-        <Link to="/"><h3 className="text-white hover:scale-110 duration-100  px-20  right-40 top-5 absolute">Projects</h3></Link>
-        <Link to="/"><h3 className="text-white hover:scale-110 duration-100 px-20  right-20  top-5 absolute">Resume</h3></Link>
+    <div className="flex flex-row  space-x-60  relative  items-center pl-9 py-9 bg-black ">
+         <Link to="/">
+         <h3 className="text-white hover:scale-110 duration-100  top-5  absolute">
+            <FontAwesomeIcon icon={faHouse} />  Home</h3></Link>
+        <Link to="/">
+        <h3 className="text-white hover:scale-110 duration-100    top-5 absolute">
+            <FontAwesomeIcon icon={faUser} /> About</h3></Link>
+        <Link to="/">
+        <h3 className="text-white hover:scale-110 duration-100     top-5 absolute">
+            <FontAwesomeIcon icon={faDiagramProject} /> Projects</h3></Link>
+        <Link to="/">
+        <h3 className="text-white hover:scale-110 duration-100  top-5 absolute">
+            <FontAwesomeIcon icon={faFile} /> Resume</h3></Link>
         <Link to="https://github.com/kishore445/IMDB_Clone.git" target="_blank">
-        <button className="text-white absolute right-10 top-5 px-20 "><FontAwesomeIcon icon={faGithub} /></button>
+        <button className="text-white absolute top-5 right-20 "><FontAwesomeIcon icon={faGithub} /></button>
         </Link>
      
         
