796796ad24792edcf8ce6fcb177f80fa65e56b13
Kishore_Tuggali <86900190+kishore445@users.noreply.github.com>
2024-06-19 19:26:39 +0530

    Git icon added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 const Navbar = () => {
   return (
     <div className="flex flex-row  relative  items-center pl-9 py-9 bg-black ">
@@ -8,7 +9,12 @@ const Navbar = () => {
         <Link to="/"><h3 className="text-white hover:scale-110 duration-100 px-20  right-60 top-5 absolute">About</h3></Link>
         <Link to="/"><h3 className="text-white hover:scale-110 duration-100  px-20  right-40 top-5 absolute">Projects</h3></Link>
         <Link to="/"><h3 className="text-white hover:scale-110 duration-100 px-20  right-20  top-5 absolute">Resume</h3></Link>
-        <button className="text-white absolute right-10 top-5 px-20 ">Git</button>
+        <Link to="https://github.com/kishore445/IMDB_Clone.git" target="_blank">
+        <button className="text-white absolute right-10 top-5 px-20 "><FontAwesomeIcon icon={faGithub} /></button>
+        </Link>
+     
+        
+        
     </div>
   )
 }
