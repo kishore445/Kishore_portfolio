d84a5ad5669c79f68ae6ad13cecc177ecf6f0166
kishore Tuggali <kishorekishore201@gmail.com>
2024-08-23 00:26:29 +0530

    Resume changes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,7 +7,7 @@ const Resume = () => {
     <div className=" md:h-[100vh]  bg-fixed text-white flex justify-center" style={{backgroundImage:`url(${image1})`}}>
         <div className='flex justify-center items-center h-[500px] mt-20'>
         
-        <a href="Kishore_portfolio-master\src\components\Resume.js" download="Resume.pdf"className='bg-slate-800 text-4xl rounded italic py-4 px-4'><h3><FontAwesomeIcon icon={faDownload} />  DownLoad Resume  </h3></a>
+        <a href="/Kishore_Tuggali.pdf" download="Resume.pdf"className='bg-slate-800 text-4xl rounded italic py-4 px-4'><h3><FontAwesomeIcon icon={faDownload} />  DownLoad Resume  </h3></a>
         </div>
 
         
