56577c2d65820db93ae6cb531d0f8f370e960495
kishore Tuggali <kishorekishore201@gmail.com>
2024-12-01 07:41:13 +0530

    new changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,14 +8,7 @@ const Home = () => {
   return (
     <div className=" md:h-[100vh] bg-fixed flex  " style={{backgroundImage:`url(${image1})`,backgroundSize: '50%',}}>
       
-        <div className="w-1/2 p-2 ml-5 " >
-      <span class=" fixed block text-sm font-medium  text-blue-500 left-2"> Need Something ? 
-      <input 
-    class="border border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 
-    contrast-more:placeholder-slate-500 w-2/2 mt-2 p-2 rounded-md  text-black ml-4 bg-fixed"
-    placeholder="Type here..." />
-      </span> 
-        </div>
+        
       
       <div >
            <div className=" text-blue-500 fixed top-40 left-2 text-black text-3xl">Hi There!
@@ -26,7 +19,7 @@ const Home = () => {
            <div className="text-blue-500 fixed bottom-5 left-2 text-black text-3xl">Senior Software Test Engineer
              </div>
          </div>
-      <div  className=" w-1/2 bg-cover flex justify-end fixed bg-cover h-screen right-0"style={{ backgroundImage: `url(${image3})`}}>   </div>       
+      <div  className=" w-1/2 bg-cover flex justify-end fixed bg-cover h-screen right-0"style={{ backgroundImage: `url(${image3})`,}}>   </div>       
         
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,29 +7,30 @@ import { faAddressBook } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row sticky top-0 space-x-60    items-center pl-9 py-9 bg-blue-700 ">
+    <div className="flex flex-col md:flex-row sticky top-0 space-x-40   items-center pl-9 py-9 bg-blue-700 ">
          <Link to="/">
-         <h3 className="text-white hover:scale-110 duration-100  top-5  absolute">
-         <FontAwesomeIcon icon={faHouse} />  Home</h3></Link>
+         <h3 className="text-white hover:scale-110 duration-100 my-2 md:my-0 top-5   ">
+         <FontAwesomeIcon icon={faHouse}className="mr-2" />  Home</h3></Link>
 
         <Link to="/About">
-        <h3 className="text-white hover:scale-110 duration-100    top-5 absolute">
-            <FontAwesomeIcon icon={faUser} /> About</h3></Link>
+        <h3 className="text-white hover:scale-110 duration-100   my-2  md:my-0 top-5 ">
+            <FontAwesomeIcon icon={faUser}className="mr-2" /> About</h3></Link>
 
         <Link to="/Projects">
-        <h3 className="text-white hover:scale-110 duration-100     top-5 absolute">
-            <FontAwesomeIcon icon={faDiagramProject} /> Projects</h3></Link>
+        <h3 className="text-white hover:scale-110 duration-100  my-2 md:my-0  top-5 ">
+            <FontAwesomeIcon icon={faDiagramProject} className="mr-2"/> Projects</h3></Link>
 
         <Link to="/Resume">
-        <h3 className="text-white hover:scale-110 duration-100  top-5 absolute">
-            <FontAwesomeIcon icon={faFile} /> Resume</h3></Link>
+        <h3 className="text-white hover:scale-110 duration-100 my-2 md:my-0 top-5 ">
+            <FontAwesomeIcon icon={faFile} className="mr-2"/> Resume</h3></Link>
 
        <Link to="/Contact">
-        <h3 className="text-white hover:scale-110 duration-100  top-5 absolute">
-        <FontAwesomeIcon icon={faAddressBook} /> Contact me</h3></Link> 
+        <h3 className="text-white hover:scale-110 duration-100 my-2  md:my-0 top-5 ">
+        <FontAwesomeIcon icon={faAddressBook} className="mr-2" /> Contact me</h3></Link> 
+      
 
-        <Link to="https://github.com/kishore445/IMDB_Clone.git " target="_blank" rel="noreferrer">
-        <button className="text-white absolute top-5 right-20 text-2xl "><FontAwesomeIcon icon={faGithub} /></button>
+        <Link to="https://github.com/kishore445/IMDB_Clone.git " target="_blank" rel="noreferrer" className='mt-4 md:mt-0 md:ml-auto'>
+        <button className="text-white absolute top-5 right-20 text-2xl  "><FontAwesomeIcon icon={faGithub} /></button>
         </Link>
      
         
